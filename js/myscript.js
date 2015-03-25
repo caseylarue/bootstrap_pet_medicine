9386a2c2b30a723a0543dff64e48bbda580f9dbd
Casey LaRue <casey.larue@gmail.com>
2015-03-24 17:44:07 -0700

    bootstrap website modified

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,45 @@
-$('.carousel').carousel({
-  interval: false
-})
+$(function() {
+
+	"use strict";
+
+	var topoffset = 50;  // variable for menu height
+
+	//activate scrollspy
+	$('body').scrollspy({
+		target: 'header .navbar',
+		offset: topoffset
+	});
+
+	// add an inbody calls to scrollspy when event fires
+	$('.navbar-fixed-top').on('activate.bs.scrollspy', function(){
+		var hash = $(this).find('li.active a').attr('href');
+		if(hash !== '#my-carousel'){
+			$('header nav').addClass('inbody');
+		}
+		else {
+			$('header nav').removeClass('inbody');
+		}
+	});
+
+	$('.carousel').carousel({
+	 interval: false
+	})
+
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+  
+});
